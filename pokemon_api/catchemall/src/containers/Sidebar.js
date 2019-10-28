efc62ae09eb5d9159b380d27a0b6d131a011da9d
maxim <max@e-i.com.ru>
2019-10-28 23:37:50 +0000

    Pokemon: more sensible function name

--- pokemon_api/catchemall/src/containers/Sidebar.js
+++ pokemon_api/catchemall/src/containers/Sidebar.js
@@ -24,17 +24,17 @@ const shuffle = ({pokemons, catchNum}) => {
   const pokemonsNumber = pokemons.length;
   let pokemonsCopy = Object.assign({}, pokemons);
   let pokemonsShuffle = [];
-  const throwBall = (pokemonsNumber) => Math.floor(Math.random()*pokemonsNumber);
+  const getRandomIndex = (pokemonsNumber) => Math.floor(Math.random()*pokemonsNumber);
   for (let i = 0; pokemonsShuffle.length < catchNum; i++) {
     // index from 0 to pokemonsNumber-1
-    let randomIndex = throwBall(pokemonsNumber);
+    let randomIndex = getRandomIndex(pokemonsNumber);
     if (pokemonsCopy[randomIndex]) {
       pokemonsShuffle.push(pokemonsCopy[randomIndex]);
       // remove it from the list to not catch it twice
       pokemonsCopy[randomIndex] = null;
     } else {
       // re-throw
-      randomIndex = throwBall(pokemonsNumber);
+      randomIndex = getRandomIndex(pokemonsNumber);
     }
   }
   return pokemonsShuffle;
