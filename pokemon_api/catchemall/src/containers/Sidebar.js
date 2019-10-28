775a24570fb81b8d99ba7d85b862ae23355b28a9
maxim <max@e-i.com.ru>
2019-10-28 22:52:09 +0000

    Pokemon: shuffle em with a helper function

--- pokemon_api/catchemall/src/containers/Sidebar.js
+++ pokemon_api/catchemall/src/containers/Sidebar.js
@@ -1,23 +1,7 @@
 import React from 'react';
 
 const Sidebar = (props) => {
-  const pokemonsNumber = props.pokemons.length;
-  let pokemons = Object.assign({}, props.pokemons);
-  let pokemonsShuffle = [];
-  for (let i = 0; pokemonsShuffle.length < props.catchNum; i++) {
-    // index from 0 to pokemonsNumber-1
-    let randomIndex = Math.floor(Math.random()*pokemonsNumber);
-    if (pokemons[randomIndex]) {
-      pokemonsShuffle.push(pokemons[randomIndex]);
-      // remove it from the list to not catch it twice
-      pokemons[randomIndex] = null;
-    } else {
-      // re-catch
-      randomIndex = Math.floor(Math.random()*pokemonsNumber);
-    }
-  }
-
-  const pokemonList = pokemonsShuffle.map((pokemon, index) =>
+  const pokemonList = shuffle({...props}).map((pokemon, index) =>
     <PokemonListItem key={index}
       value={pokemon.name} />
   );
@@ -34,6 +18,26 @@ const Sidebar = (props) => {
 const PokemonListItem = (props) => (
   <li>{props.value}</li>
 );
+
+const shuffle = ({pokemons, catchNum}) => {
+  const pokemonsNumber = pokemons.length;
+  let pokemonsCopy = Object.assign({}, pokemons);
+  let pokemonsShuffle = [];
+  const throwBall = (pokemonsNumber) => Math.floor(Math.random()*pokemonsNumber);
+  for (let i = 0; pokemonsShuffle.length < catchNum; i++) {
+    // index from 0 to pokemonsNumber-1
+    let randomIndex = throwBall(pokemonsNumber);
+    if (pokemonsCopy[randomIndex]) {
+      pokemonsShuffle.push(pokemonsCopy[randomIndex]);
+      // remove it from the list to not catch it twice
+      pokemonsCopy[randomIndex] = null;
+    } else {
+      // re-throw
+      randomIndex = throwBall(pokemonsNumber);
+    }
+  }
+  return pokemonsShuffle;
+}
 
 export default Sidebar;
 
